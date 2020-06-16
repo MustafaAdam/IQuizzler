374c27d2982031d9cbe4d4a5739a0bf64854d903
Mustafa <aweel017@gmail.com>
2020-06-16 19:09:14 +0400

    preparing to publish app

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'IQuizzler',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         textTheme: TextTheme(
